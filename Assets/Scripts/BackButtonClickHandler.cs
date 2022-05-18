2a0c483161055d9b50828ef2869393c901fce5fa
Nora Gyopar Murakozy <murakozy.nora@gmail.com>
2022-05-18 22:17:09 +0200

    Add back button on screens

---
+++ Assets/Scripts/BackButtonClickHandler.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DefaultNamespace
+{
+    public class BackButtonClickHandler : MonoBehaviour
+    {
+        public void OnClick()
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
